0e0c14c1d61031b6894e0971f55a95fb44f8b083
romeo <romeo.semper@marmara.at>
2018-08-31 12:05:01 +0200

    =first

---
+++ src/MarmaraIT/LaravelDynamicAttributes/DynamicAttributesProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MarmaraIT\LaravelDynamicAttributes;
+
+use Illuminate\Support\ServiceProvider;
+
+class DynamicAttributesProvider extends ServiceProvider{
+
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
